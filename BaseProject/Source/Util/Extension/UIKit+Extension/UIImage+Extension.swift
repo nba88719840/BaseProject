087741340b098ffc28efc68da2ac840b0432b6c2
TangXiaoDe <xiaodehappy@foxmail.com>
2021-02-25 16:50:48 +0800

    Util: UIImage添加扩展方法

--- BaseProject/Source/Util/Extension/UIKit+Extension/UIImage+Extension.swift
+++ BaseProject/Source/Util/Extension/UIKit+Extension/UIImage+Extension.swift
@@ -13,5 +13,28 @@ import UIKit
 
 extension UIImage {
 
+    /// 根据坐标获取图片中的像素颜色值
+    subscript (x: Int, y: Int) -> UIColor? {
+        guard let provider = self.cgImage?.dataProvider, let providerData = provider.data, let data = CFDataGetBytePtr(providerData) else {
+            return nil
+        }
+        if x < 0 || x > Int(size.width) || y < 0 || y > Int(size.height) {
+            return nil
+        }
+        //
+        let numberOfComponents = 4
+        let pixelData = ((Int(size.width) * y) + x) * numberOfComponents
+        //
+        let r = CGFloat(data[pixelData + 0]) / 255.0
+        let g = CGFloat(data[pixelData + 1]) / 255.0
+        let b = CGFloat(data[pixelData + 2]) / 255.0
+        let a = CGFloat(data[pixelData + 3]) / 255.0
+        
+        return UIColor.init(red: r, green: g, blue: b, alpha: a)
+    }
+    ///
+    func getColor(at point: CGPoint) -> UIColor? {
+        return self[Int(point.x), Int(point.y)]
+    }
 
 }
